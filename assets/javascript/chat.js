7ab8e843db5d83f9035f32eac4f67663f813cff9
DESKTOP-P02URSR\John <jmcarroll95@gmail.com>
2018-01-25 20:34:18 -0500

    added a test html for Firebase connection

--- assets/javascript/chat.js
+++ assets/javascript/chat.js
@@ -1,23 +1,41 @@
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyBV_0I14cCrwUXBLhRC-giTDnquAxCHJVI",
-    authDomain: "twitter-troll.firebaseapp.com",
-    databaseURL: "https://twitter-troll.firebaseio.com",
-    projectId: "twitter-troll",
-    storageBucket: "twitter-troll.appspot.com",
-    messagingSenderId: "888156273211"
-  };
-  firebase.initializeApp(config);
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyBV_0I14cCrwUXBLhRC-giTDnquAxCHJVI",
+authDomain: "twitter-troll.firebaseapp.com",
+databaseURL: "https://twitter-troll.firebaseio.com",
+projectId: "twitter-troll",
+storageBucket: "twitter-troll.appspot.com",
+messagingSenderId: "888156273211"
+};
+firebase.initializeApp(config);
 
-  var database = firebase.database();
+var database = firebase.database();
 
-  var chatMessage = 'This is totally a chat!';
+var chatMessage = 'This is totally a chat!';
 
-  $("#click-button").on("click", function() {
-
-    //  Store Click Data to Firebase in a JSON property called clickCount
+$("#send-message").on("click", function() {
+    //  Store Message Data to Firebase in a JSON property called chat_message
     // Note how we are using the Firebase .set() method
     database.ref().set({
-      clickCount: chatMessage
+        chat_message: chatMessage
     });
-  });
+});
+
+database.ref().on("value", function(snapshot) {
+
+    // Then we console.log the value of snapshot
+    console.log(snapshot.val());
+
+    // Then we change the html associated with the number.
+    $("#click-value").text(snapshot.val().chat_message);
+
+    // Then update the chatMessage variable with data from the database.
+    chatMessage = snapshot.val().chat_message;
+
+  // If there is an error that Firebase runs into -- it will be stored in the "errorObject"
+  // Again we could have named errorObject anything we wanted.
+  }, function(errorObject) {
+
+    // In case of error this will print the error
+    console.log("The read failed: " + errorObject.code);
+});
