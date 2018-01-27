d8e6f6d2dcf4b8b6fce854679b065eb21ea11140
DESKTOP-P02URSR\John <jmcarroll95@gmail.com>
2018-01-27 12:50:28 -0500

    added sequelize structure/jamal's work

---
+++ config/keys.js
@@ -0,0 +1,11 @@
+console.log('this is loaded');
+
+ 
+var twitterKeys = {
+  consumer_key: 'f8EQFvRSeN8jiJCbmEtUAo0hY',
+  consumer_secret: 'zZilvXsV2HxFWRjJ0vH7M0TJR2yL6TtaPkYxudT2fTOtjk5w9c',
+  access_token_key: '936974460275609600-hsMPiENo87ZZ7MFzmihRcgURYwKv5Oe',
+  access_token_secret: 'mqs0YC6hYD1bIL9WGNkJZfJ5CX7QL6LKDSV200h84Js5g'
+};
+
+module.exports = twitterKeys;
