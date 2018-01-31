5772d2d5e155512840162c434348eae3a0ce3ba7
DESKTOP-P02URSR\John <jmcarroll95@gmail.com>
2018-01-30 20:40:11 -0500

    renamed a file, and updated routing to include sign-in page

--- config/orm.js
+++ config/orm.js
@@ -23,14 +23,10 @@ function objToSql(ob) {
   // loop through the keys and push the key/value as a string int arr
   for (var key in ob) {
     var value = ob[key];
-    // check to skip hidden properties
     if (Object.hasOwnProperty.call(ob, key)) {
-      // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
       if (typeof value === "string" && value.indexOf(" ") >= 0) {
         value = "'" + value + "'";
       }
-      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-      // e.g. {sleepy: true} => ["sleepy=true"]
       arr.push(key + "=" + value);
     }
   }
