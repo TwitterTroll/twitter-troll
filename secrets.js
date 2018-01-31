065fe47c6c32d117396f7c03f44c31ed60fdabcf
NealRosenberger <nealarosenberger@gmail.com>
2018-01-30 20:36:50 -0500

    please work

---
+++ secrets.js
@@ -0,0 +1,8 @@
+var Secrets = function () {
+    this.consumerKey = 'f8EQFvRSeN8jiJCbmEtUAo0hY';
+    this.consumerSecret = 'zZilvXsV2HxFWRjJ0vH7M0TJR2yL6TtaPkYxudT2fTOtjk5w9c';
+    this.accessTokenKey = '936974460275609600-hsMPiENo87ZZ7MFzmihRcgURYwKv5Oe';
+    this.accessTokenSecret = 'mqs0YC6hYD1bIL9WGNkJZfJ5CX7QL6LKDSV200h84Js5g';
+}
+
+module.exports = Secrets;
