b1e38e3d4a6645efdb3ee25ed2839ec3841d29b8
Jamal Patterson <jamal@Jamals-MacBook-Pro.local>
2018-01-31 21:08:47 -0500

    Added twitter function

--- config/keys.js
+++ config/keys.js
@@ -1,13 +1,26 @@
-var Secrets = require('../secrets.js');
-
 console.log('this is loaded');
 
  
 var twitterKeys = {
-  consumer_key: Secrets.consumerKey,
-  consumer_secret: Secrets.consumerSecret,
-  access_token_key: Secrets.accessTokenKey,
-  access_token_secret: Secrets.accessTokenSecret
+  consumer_key: 'nIS4TmQDDEudv00HEo2cSu0Dx',
+  consumer_secret: 'w0ikvA09Tx6v0ujnmNbIJeHE4SNKjiSegYjeYs6dV9wRmVV13y',
+  access_token_key: '936974460275609600-tPDEKS2i8pM5Q4eqs7cthdRAaQJJlFK',
+  access_token_secret: '5wXxFNZheDZcYaqOIHbXGPCFggDBsgcWJpDN0iA4m2Sue'
 };
 
 module.exports = twitterKeys;
+
+
+// var Secrets = require('../secrets.js');
+
+// console.log('this is loaded');
+
+ 
+// var twitterKeys = {
+//   consumer_key: Secrets.consumerKey,
+//   consumer_secret: Secrets.consumerSecret,
+//   access_token_key: Secrets.accessTokenKey,
+//   access_token_secret: Secrets.accessTokenSecret
+// };
+
+// module.exports = twitterKeys;
--- controllers/tweets.js
+++ controllers/tweets.js
@@ -1,33 +1,33 @@
-// Import the model (burger.js) to use its database functions.
-var tweet = require("../models/tweet.js");
+// // Import the model (burger.js) to use its database functions.
+// var tweet = require("../models/tweet.js");
 
-// module.exports = {
-//   function tweet() {
- var twitterAPI =  module.exports = function() {
-    var fs = require("fs");
-    var Twitter = require('twitter');
-    var keys = require("../config/keys.js");
-    var client = new Twitter(keys);
-  //input for twitter name
-  //var name = process.argv[3];
+// // module.exports = {
+// //   function tweet() {
+//  var twitterAPI =  module.exports = function() {
+//     var fs = require("fs");
+//     var Twitter = require('twitter');
+//     var keys = require("../config/keys.js");
+//     var client = new Twitter(keys);
+//   //input for twitter name
+//   //var name = process.argv[3];
 
-  var tweet = require(“../models/tweet.js”);
-   var fs = require(“fs”);
-   var Twitter = require(‘twitter’);
-   var keys = require(“../config/keys.js”);
-   var client = new Twitter(keys);
-   module.exports = function tweetIt() {
+//   var tweet = require(“../models/tweet.js”);
+//    var fs = require(“fs”);
+//    var Twitter = require(‘twitter’);
+//    var keys = require(“../config/keys.js”);
+//    var client = new Twitter(keys);
+//    module.exports = function tweetIt() {
  
- var params = {screen_name: “pattersonluri”, count:21};
+//  var params = {screen_name: “pattersonluri”, count:21};
  
-   client.get(‘statuses/user_timeline’, params, function(error, tweets, data) {
+//    client.get(‘statuses/user_timeline’, params, function(error, tweets, data) {
      
        
-       for (var i=1; i<tweets.length; i++){ 
-           tweet = JSON.stringify(tweets[i].text);
+//        for (var i=1; i<tweets.length; i++){ 
+//            tweet = JSON.stringify(tweets[i].text);
          
-          }
+//           }
            
- });
+//  });
  
-}
+// }
